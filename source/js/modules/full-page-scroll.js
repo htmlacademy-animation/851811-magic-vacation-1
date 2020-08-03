40ee5334f32305c59cb375261499788c206ed9c3
z.salikhkulova <z.salikhkulova@fun-box.ru>
2020-08-03 21:29:39 +0500

    Возможная правка двойного проигрывания

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -70,6 +70,10 @@ export default class FullPageScroll {
         }
       });
 
+      document.querySelector(`.prizes__item--journeys img`).src = ''
+      document.querySelector(`.prizes__item--cases img`).src = ''
+      document.querySelector(`.prizes__item--codes img`).src = ''
+
       setTimeout(() => {
         this.startSvgAnimation({element: document.querySelector(`.prizes__item--journeys`), activeClass: `prizes__item--active`, svgFile: `img/primary-award-animation.svg`});
       }, 0);
@@ -113,9 +117,11 @@ export default class FullPageScroll {
   }
 
   startSvgAnimation({element, activeClass, svgFile}) {
-    element.classList.add(activeClass);
-
     const imageElement = element.querySelector(`img`);
     imageElement.src = `${svgFile}?${new Date().getTime()}`;
+
+    setTimeout(() => {
+      element.classList.add(activeClass);
+    }, 0);
   }
 }
