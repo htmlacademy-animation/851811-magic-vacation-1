c2fdce97b9a95b578573b718372041c6e29f8573
Keks <keks@htmlacademy.ru>
2020-08-10 17:28:10 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/script.js
+++ 10/js/script.js
@@ -10197,13 +10197,23 @@ const createOpacitySet = ({id, to, begin}) => setAnimationAttributes({
 
 const createVictoryAnimation = (element) => {
   const id = element.id.replace(/-/g, `_`);
-  const firstAnimationId = `${id}_scale`;
+  const firstAnimationId = `${id}_opacity`;
+
+  element.setAttribute(`style`, `opacity: 0;`);
+
+  element.appendChild(
+      createOpacitySet({
+        id: firstAnimationId,
+        to: 1,
+        begin: `indefinite`,
+      })
+  );
 
   element.appendChild(
       createTransformAnimation({
-        id: firstAnimationId,
+        id: `${id}_scale`,
         values: SCALE,
-        begin: `indefinite`
+        begin: `${firstAnimationId}.begin`
       })
   );
 
