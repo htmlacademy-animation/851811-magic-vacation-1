d3a88662c90c66a9858f06740e048e2d649fc47d
z.salikhkulova <z.salikhkulova@fun-box.ru>
2020-10-15 12:35:58 +0500

    Перенос glsl кода в отдельные файлы

---
+++ source/js/modules/three/common/fragment-shader.glsl
@@ -0,0 +1,29 @@
+precision mediump float;
+
+uniform sampler2D map;
+
+struct optionsStruct {
+  float hueShift;
+};
+
+uniform optionsStruct options;
+
+varying vec2 vUv;
+
+vec3 hueShift(vec3 color, float hue) {
+  const vec3 k = vec3(0.57735, 0.57735, 0.57735);
+  float cosAngle = cos(hue);
+  return vec3(color * cosAngle + cross(k, color) * sin(hue) + k * dot(k, color) * (1.0 - cosAngle));
+}
+
+void main() {
+  vec4 texel = texture2D(map, vUv);
+  vec4 result = texel;
+
+  if (options.hueShift != 0.0) {
+    vec3 hueShifted = hueShift(result.rgb, options.hueShift);
+
+    result = vec4(hueShifted.rgb, 1);
+  }
+  gl_FragColor = result;
+}
--- source/js/modules/three/common/get-raw-shader-material-attrs.js
+++ source/js/modules/three/common/get-raw-shader-material-attrs.js
@@ -1,58 +1,10 @@
+import vertexShader from './vertex-shader.glsl';
+import fragmentShader from './fragment-shader.glsl';
+
 export default (uniforms) => (
   {
     uniforms,
-    vertexShader: `
-      // Переменные, которые передаёт Three.js для проецирования на плоскость
-      uniform mat4 projectionMatrix;
-      uniform mat4 modelMatrix;
-      uniform mat4 viewMatrix;
-
-      // Атрибуты вершины из геометрии
-      attribute vec3 position;
-      attribute vec3 normal;
-      attribute vec2 uv;
-
-      // Varying-переменная для передачи uv во фрагментный шейдер
-      varying vec2 vUv;
-
-      void main() {
-
-        vUv = uv;
-
-        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 );
-
-      }
-    `,
-    fragmentShader: `
-      precision mediump float;
-
-      uniform sampler2D map;
-
-      struct optionsStruct {
-        float hueShift;
-      };
-
-      uniform optionsStruct options;
-
-      varying vec2 vUv;
-
-      vec3 hueShift(vec3 color, float hue) {
-        const vec3 k = vec3(0.57735, 0.57735, 0.57735);
-        float cosAngle = cos(hue);
-        return vec3(color * cosAngle + cross(k, color) * sin(hue) + k * dot(k, color) * (1.0 - cosAngle));
-      }
-
-      void main() {
-        vec4 texel = texture2D(map, vUv);
-        vec4 result = texel;
-
-        if (options.hueShift != 0.0) {
-          vec3 hueShifted = hueShift(result.rgb, options.hueShift);
-
-          result = vec4(hueShifted.rgb, 1);
-        }
-        gl_FragColor = result;
-      }
-    `,
+    vertexShader,
+    fragmentShader,
   }
 );
