720acdcf77b652ce3bc9143b3a758d1ae9f2bdc3
z.salikhkulova <z.salikhkulova@fun-box.ru>
2020-10-17 12:32:52 +0500

    Правки по комментариям в предыдущем PR

--- source/js/modules/three/common/fragment-shader.glsl
+++ source/js/modules/three/common/fragment-shader.glsl
@@ -11,7 +11,7 @@ uniform optionsStruct options;
 varying vec2 vUv;
 
 vec3 hueShift(vec3 color, float hue) {
-  const vec3 k = vec3(0.57735, 0.57735, 0.57735);
+  const vec3 k = vec3(0.57735);
   float cosAngle = cos(hue);
   return vec3(color * cosAngle + cross(k, color) * sin(hue) + k * dot(k, color) * (1.0 - cosAngle));
 }
@@ -20,10 +20,8 @@ void main() {
   vec4 texel = texture2D(map, vUv);
   vec4 result = texel;
 
-  if (options.hueShift != 0.0) {
-    vec3 hueShifted = hueShift(result.rgb, options.hueShift);
+  vec3 hueShifted = hueShift(result.rgb, options.hueShift);
+  result = vec4(hueShifted.rgb, 1);
 
-    result = vec4(hueShifted.rgb, 1);
-  }
   gl_FragColor = result;
 }
