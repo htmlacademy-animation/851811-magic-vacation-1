b950e2682c9fefd54594f0f90862e9fca38e3b57
z.salikhkulova <z.salikhkulova@fun-box.ru>
2020-10-17 12:34:39 +0500

    Добавление пузырьков с эффектом лупы

--- source/js/modules/three/common/fragment-shader.glsl
+++ source/js/modules/three/common/fragment-shader.glsl
@@ -4,9 +4,22 @@ uniform sampler2D map;
 
 struct optionsStruct {
   float hueShift;
+  bool magnify;
 };
 
 uniform optionsStruct options;
+
+struct bubbleStruct {
+  float radius;
+  vec2 position;
+};
+
+struct magnificationStruct {
+  bubbleStruct bubbles[3];
+  vec2 resolution;
+};
+
+uniform magnificationStruct magnification;
 
 varying vec2 vUv;
 
@@ -16,9 +29,76 @@ vec3 hueShift(vec3 color, float hue) {
   return vec3(color * cosAngle + cross(k, color) * sin(hue) + k * dot(k, color) * (1.0 - cosAngle));
 }
 
+float getOffset(vec2 point, vec2 circle) {
+  return sqrt(pow(point.x - circle.x, 2.0) + pow(point.y - circle.y, 2.0));
+}
+
+bool isCurrentBubble(vec2 point, vec2 circle, float radius, float outlineThickness) {
+  float offset = getOffset(point, circle);
+  return offset < radius + outlineThickness;
+}
+
+
+bool isInsideTheCircle(vec2 point, vec2 circle, float radius) {
+  float offset = getOffset(point, circle);
+  return offset < radius;
+}
+
+bool isOutlineOfTheCircle(vec2 point, vec2 circle, float radius, float outlineThickness) {
+  float offset = getOffset(point, circle);
+  return floor(offset) >= floor(radius) && floor(offset) <= floor(radius + outlineThickness);
+}
+
+vec4 blendOutline(vec4 texture, vec4 outline) {
+  return vec4(mix(texture.rgb, outline.rgb, outline.a), texture.a);
+}
+
+vec4 magnify(sampler2D map, magnificationStruct magnification) {
+  float outlineThickness = 3.0;
+  vec4 outlineColor = vec4(1, 1, 1, 0.3);
+
+  vec2 resolution = magnification.resolution;
+  bubbleStruct bubble = magnification.bubbles[0];
+  vec2 point = gl_FragCoord.xy;
+
+  for (int index = 0; index < 3; index++) {
+    bubbleStruct currentBubble = magnification.bubbles[index];
+
+    vec2 currentPosition = currentBubble.position;
+    float currentRadius = currentBubble.radius;
+
+    if (isCurrentBubble(point, currentPosition, currentRadius, outlineThickness)) {
+      bubble = currentBubble;
+    }
+  }
+
+  vec2 position = bubble.position;
+  float radius = bubble.radius;
+  float h = bubble.radius / 2.0;
+
+  float hr = radius * sqrt(1.0 - pow((radius - h) / radius, 2.0));
+  float offset = sqrt(pow(point.x - position.x, 2.0) + pow(point.y - position.y, 2.0));
+
+  bool pointIsInside = isInsideTheCircle(point, position, hr);
+  bool pointIsOutline = isOutlineOfTheCircle(point, position, hr, outlineThickness);
+
+  vec2 newPoint = pointIsInside ? (point - position) * (radius - h) / sqrt(pow(radius, 2.0) - pow(offset, 2.0)) + position : point;
+
+  vec2 newVUv = (newPoint) / resolution;
+
+  if (pointIsOutline) {
+    return blendOutline(texture2D(map, newVUv), outlineColor);
+  }
+
+  return texture2D(map, newVUv);
+}
+
 void main() {
-  vec4 texel = texture2D(map, vUv);
-  vec4 result = texel;
+  vec4 result = texture2D(map, vUv);
+
+  if (options.magnify) {
+    result = magnify(map, magnification);
+  }
 
   vec3 hueShifted = hueShift(result.rgb, options.hueShift);
   result = vec4(hueShifted.rgb, 1);
