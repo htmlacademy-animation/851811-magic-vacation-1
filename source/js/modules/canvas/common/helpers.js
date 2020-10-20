5692cffb75f53a013b901518a0283e0b41e8b4cf
z.salikhkulova <z.salikhkulova@fun-box.ru>
2020-10-20 13:59:37 +0500

    Добавление хелпера для анимации с определенной частотой кадров

--- source/js/modules/canvas/common/helpers.js
+++ source/js/modules/canvas/common/helpers.js
@@ -64,6 +64,40 @@ export const animateEasing = (render, duration, easing) => new Promise((resolve)
   }());
 });
 
+export const animateEasingWithFramerate = (render, duration, easing, framerate = 1000 / 25) => new Promise((resolve) => {
+  let start = null;
+  let lastFrameUpdateTime = null;
+  let timePassedSinceLastUpdate = null;
+
+  function loop(currentTime) {
+    if (!start) {
+      start = currentTime;
+    }
+
+    if (!lastFrameUpdateTime) {
+      lastFrameUpdateTime = currentTime;
+    }
+
+    let progress = (currentTime - start) / duration;
+    if (progress > 1) {
+      render(easing(1));
+      resolve(true);
+      return;
+    }
+
+    timePassedSinceLastUpdate = currentTime - lastFrameUpdateTime;
+    if (timePassedSinceLastUpdate > framerate) {
+      lastFrameUpdateTime = currentTime;
+
+      render(easing(progress));
+    }
+
+    requestAnimationFrame(loop);
+  }
+
+  requestAnimationFrame(loop);
+});
+
 export const runSerial = async (tasks) => {
   let result = Promise.resolve();
   tasks.forEach((task) => {
