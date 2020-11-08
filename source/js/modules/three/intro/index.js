fc2f9dc1e142db831efcdf6221577ce7179b716a
z.salikhkulova <z.salikhkulova@fun-box.ru>
2020-11-08 19:30:15 +0500

    Расположение объектов на первом экране

--- source/js/modules/three/intro/index.js
+++ source/js/modules/three/intro/index.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 
 import getRawShaderMaterialAttrs from '../common/hue-and-bubbles-raw-shader';
 
+import SVGObject from '../common/svg-object';
+
 export default class Intro {
   constructor() {
     this.innerWidth = window.innerWidth;
@@ -58,7 +60,52 @@ export default class Intro {
       this.scene.add(image);
     };
 
+    this.addSvgObjects();
+
     this.animationRequest = requestAnimationFrame(this.render);
+  }
+
+  addSvgObjects() {
+    this.loadKeyhole();
+    this.loadFlamingo();
+    this.loadSnowflake();
+    this.loadQuestion();
+    this.loadLeaf();
+  }
+
+  async loadKeyhole() {
+    const keyhole = await new SVGObject({name: `keyhole`}).getObject();
+    keyhole.position.set(-1000, 1000, 10);
+    this.scene.add(keyhole);
+  }
+
+  async loadFlamingo() {
+    const flamingo = await new SVGObject({name: `flamingo`}).getObject();
+    flamingo.position.set(-350, 300, 100);
+    flamingo.scale.set(-2, 2, 2);
+    flamingo.rotation.copy(new THREE.Euler(20 * THREE.Math.DEG2RAD, 0, 0), `XYZ`);
+    this.scene.add(flamingo);
+  }
+
+  async loadSnowflake() {
+    const snowflake = await new SVGObject({name: `snowflake`}).getObject();
+    snowflake.position.set(-350, 0, 100);
+    snowflake.scale.set(1.2, 1.2, 1.2);
+    snowflake.rotation.copy(new THREE.Euler(20 * THREE.Math.DEG2RAD, 40 * THREE.Math.DEG2RAD, 0), `XYZ`);
+    this.scene.add(snowflake);
+  }
+
+  async loadQuestion() {
+    const question = await new SVGObject({name: `question`}).getObject();
+    question.position.set(150, -200, 100);
+    this.scene.add(question);
+  }
+
+  async loadLeaf() {
+    const leaf = await new SVGObject({name: `leaf-1`}).getObject();
+    leaf.position.set(400, 200, 100);
+    leaf.scale.set(-1.2, 1.2, 1.2);
+    this.scene.add(leaf);
   }
 
   end() {
