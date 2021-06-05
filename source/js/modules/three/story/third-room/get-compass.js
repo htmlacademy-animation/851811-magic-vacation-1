e0f724cfe4b81395e3551430319a52595968766f
z.salikhkulova <z.salikhkulova@fun-box.ru>
2021-06-05 17:36:24 +0500

    Анимация стрелки компаса

---
+++ source/js/modules/three/story/third-room/get-compass.js
@@ -0,0 +1,78 @@
+import {loadModel} from '../../common/load-model';
+import {setMeshParams, getOriginalRotation} from '../../common/helpers';
+import {isMobile} from '../../../helpers';
+import bezierEasing from '../../../canvas/common/bezier-easing';
+import {animateEasingWithFramerate} from '../../../canvas/common/helpers';
+
+const linear = bezierEasing(0.0, 0.0, 1.0, 1.0);
+
+const count = 4;
+
+const tailAnimationSettings = {
+  rotate: {
+    min: 0,
+    max: 30,
+  },
+  easing: linear,
+  duration: count * 2 * 1000,
+};
+
+const compassParams = {
+  name: `compass`,
+  type: `gltf`,
+  path: `img/models/compass.gltf`,
+  scale: 0.3,
+  position: {x: 0, y: 0, z: 0},
+  rotate: {x: 0, y: -45, z: 0},
+  ...!isMobile && {
+    receiveShadow: true,
+    castShadow: true,
+  }
+};
+
+export default (callback) => {
+  const compassAnimationTick = (object, originalRotation) => {
+    return (progress) => {
+      const params = calcCompassParams(progress, originalRotation);
+      setMeshParams(object, params);
+    };
+  };
+
+  const animateCompass = (object) => {
+    if (!object) {
+      return;
+    }
+
+    const hand = getHand(object);
+    const originalHandRotation = getOriginalRotation(hand);
+
+    const {duration, easing} = tailAnimationSettings;
+    animateEasingWithFramerate(compassAnimationTick(hand, originalHandRotation), duration, easing);
+  };
+
+
+  loadModel(compassParams, null, (mesh) => {
+    mesh.name = compassParams.name;
+    setMeshParams(mesh, compassParams);
+    callback(mesh, animateCompass);
+  });
+};
+
+function calcCompassParams(progress, originalRotation) {
+  return {
+    rotate: getCompassRotate(progress, originalRotation),
+  };
+}
+
+function getCompassRotate(progress, originalRotation) {
+  const {rotate} = tailAnimationSettings;
+  const amplitude = rotate.max - rotate.min;
+  const sine = Math.sin(progress * Math.PI * count) + Math.sin(progress * Math.PI * 2);
+  const z = amplitude * sine + originalRotation.z * progress;
+
+  return {...originalRotation, z};
+}
+
+function getHand(compass) {
+  return compass.children[0].getObjectByName(`ArrowCenter`);
+}
